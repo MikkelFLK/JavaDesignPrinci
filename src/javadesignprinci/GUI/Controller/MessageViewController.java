bae51578b0411fd949ce4f2eaee5bec484e8e188
Mikkel K <mikk6578@easv365.dk>
2018-03-05 12:20:37 +0100

    Initial commit

---
+++ src/javadesignprinci/GUI/Controller/MessageViewController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadesignprinci.GUI.Controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mikkel
+ */
+public class MessageViewController implements Initializable
+{
+
+    @FXML
+    private JFXTextArea messageHistory;
+    @FXML
+    private TextField messageField;
+    @FXML
+    private JFXButton btnSendMessage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+
+    @FXML
+    private void sendMessage(ActionEvent event)
+    {
+    }
+    
+}
