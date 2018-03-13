75794a66e041eff53d936bca1c2f71dc6a6b9f85
Frede94 <Frede94@users.noreply.github.com>
2018-03-13 11:32:00 +0100

    ...

--- src/javadesignprinci/GUI/Controller/MessageViewController.java
+++ src/javadesignprinci/GUI/Controller/MessageViewController.java
@@ -6,20 +6,23 @@
 package javadesignprinci.GUI.Controller;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextArea;
-import java.io.FileWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Stack;
 import javadesignprinci.BE.Message;
 import javadesignprinci.BLL.exceptions.BllException;
+import javadesignprinci.GUI.Model.Command.ICommand;
 import javadesignprinci.GUI.Model.MessageLogModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -28,15 +31,21 @@ import javafx.scene.control.TextField;
  */
 public class MessageViewController implements Initializable
 {
-
+    
     @FXML
     private TextField messageField;
     @FXML
     private JFXButton btnSendMessage;
     @FXML
     private ListView<Message> messageLog;
-
+    
     private MessageLogModel msgModel = new MessageLogModel();
+    
+    private Stack<ICommand> undos;
+    private Stack<ICommand> redos;
+    
+    private KeyCombination keysUndo;
+    private KeyCombination keysRedo;
 
     /**
      * Initializes the controller class.
@@ -44,11 +53,51 @@ public class MessageViewController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        keysRedo = new KeyCodeCombination(KeyCode.Y, KeyCodeCombination.CONTROL_DOWN);
+        keysUndo = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+        
         messageLog.setItems(msgModel.getAllMessages());
         msgModel.getAllMessages();
-
+        
+//        try
+//        {
+//            msgModel = new MessageLogModel();
+//            undos = new Stack<>();
+//            redos = new Stack<>();
+//            messageLog.setCellFactory(new Callback<ListView<Message>, ListCell<Message>>()
+//            {
+//                @Override
+//                public ListCell<Message> call(ListView<Message> param)
+//                {
+//                    ListCell<Message> cell = new ListCell<Message>()
+//                    {
+//                        @Override
+//                        protected void updateItem(Message item, boolean empty)
+//                        {
+//                            super.updateItem(item, empty);
+//                            if (!empty && item != null)
+//                            {
+//                                setText("#" + item.getId() + ": " + item.getText());
+//                            } else
+//                            {
+//                                setText(null);
+//                                setGraphic(null);
+//                            }
+//                        }
+//                    };
+//                    return cell;
+//                }
+//            });
+//            messageLog.setItems(msgModel.getAllMessages());
+//            initKeyHandling();
+//            
+//        } catch (BllException ex)
+//        {
+//            displayException(ex);
+//        }
+        
     }
-
+    
     @FXML
     private void sendMessage(ActionEvent event) throws BllException
     {
@@ -62,11 +111,16 @@ public class MessageViewController implements Initializable
         }
         messageLog.setItems(msgModel.getAllMessages());
     }
-
+    
     private void displayException(BllException ex)
     {
         System.out.println("I should display an error message wit the text: " + ex.getMessage());
         ex.printStackTrace();
     }
-
+    
+    private void initKeyHandling()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
